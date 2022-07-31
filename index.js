62b0007bbd42f8e3931ea98f1b9ba45bed4aafc5
harry <iui9@qq.com>
2022-07-31 12:40:10 +0800

    fix:部署失败问题

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 /*
  * @Author: Harry
- * @Date: 2022-07-31 11:56:12
+ * @Date: 2022-07-31 11:47:00
  * @LastEditors: harry
  * @Github: https://github.com/rr210
- * @LastEditTime: 2022-07-31 12:05:20
- * @FilePath: \markdown-it\index.js
+ * @LastEditTime: 2022-07-31 11:58:08
+ * @FilePath: \markdown-it\lib\index.js
  */
-const mdItCustomAttrs = require('./lib/index')
+'use strict';
 
-export default mdItCustomAttrs
+module.exports = function MdCustomAttrPugin(md, type, mdOptions = null) {
+  var defaultRenderer = md.renderer.rules[type];
+
+  md.renderer.rules[type] = function (tokens, idx, options, env, self) {
+    var token = tokens[idx];
+    if (mdOptions) {
+      for (let i in mdOptions) {
+        token.attrSet(i, mdOptions[i])
+      }
+    }
+    return defaultRenderer(tokens, idx, options, env, self);
+  };
+};
