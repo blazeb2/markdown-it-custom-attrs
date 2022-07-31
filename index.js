a61582a115aa333a3085fe39271af17820a32b75
harry <iui9@qq.com>
2022-07-31 11:59:27 +0800

    feat:上传库

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Harry
+ * @Date: 2022-07-31 11:56:12
+ * @LastEditors: harry
+ * @Github: https://github.com/rr210
+ * @LastEditTime: 2022-07-31 11:58:01
+ * @FilePath: \markdown-it\index.js
+ */
+const MdCustomAttrPugin = require('./lib/index')
+
+export default MdCustomAttrPugin
